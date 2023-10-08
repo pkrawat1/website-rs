dd9224077cdca3d1b0c56164b48319340671cd14
pkrawat1 <pankajrawat19sept@gmail.com>
2023-10-08 17:16:43 +0530

    add blogs page

--- src/pages/blogs.rs
+++ src/pages/blogs.rs
@@ -30,79 +30,99 @@ pub async fn blog_feeds(tag: Option<String>) -> Result<Vec<Feed>, ServerFnError>
 
   let client = Client::new()
       .get(url);
-      
+
 
     let response = client.send().await?;
 
     match response.status() {
-        StatusCode::OK | StatusCode::CREATED | StatusCode::ACCEPTED => 
+        StatusCode::OK | StatusCode::CREATED | StatusCode::ACCEPTED =>
           Ok(response.json::<BlogFeedsResponse>().await?.items)
        ,
-        _ => 
+        _ =>
           Err(ServerFnError::ServerError(String::from("Unable to load blog feeds")))
-        
+
     }
 }
 
 #[component]
 pub fn BlogsPage(cx: Scope) -> impl IntoView {
-    let (feeds, set_feeds) = create_signal(cx, vec![]);
-    view! {cx, 
-      <div>
-        Total
-        <button 
-          on:click=move |_| {
-            spawn_local(async move {
-                  match blog_feeds(None).await {
-                    Ok(blog_feeds) => set_feeds(blog_feeds),
-                    Err(_) => set_feeds(vec![])
-                  }
-                });
-          }
-        >
-          Feeds
-        </button>
-      </div>
+    let load_feeds = create_resource(cx, || (), |_| async move { 
+      match blog_feeds(None).await {
+        Ok(blog_feeds) => blog_feeds,
+        Err(_) => vec![]
+      }
+    });
+
+    view! {cx,
       <section class="">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-                <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
-                <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
-            </div> 
-            <div class="grid gap-8 lg:grid-cols-2">
-                {
-                  move || { feeds.get().into_iter()
-                    .map(|feed| view! {cx,
-                      <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                          <div class="flex justify-between items-center mb-5 text-gray-500">
-                              <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                  <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                                  "Article"
-                              </span>
-                              <span class="text-sm">{feed.pubDate}</span>
-                          </div>
-                          <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{feed.title}</a></h2>
-                          <p class="mb-5 font-light text-gray-500 dark:text-gray-400" inner_html=blog_description(feed.description)></p>
-                          <div class="flex justify-between items-center">
-                              <div class="flex items-center space-x-4">
-                                  <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                                  <span class="font-medium dark:text-white">
-                                    {feed.author}
-                                  </span>
-                              </div>
-                              <a href=feed.link class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                                  Read more
-                                  <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                              </a>
-                          </div>
-                      </article> 
-                    })
-                    .collect_view(cx) }
+                <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">"Our Blog"</h2>
+                <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">"Innovative Tech Perspectives: Discover the Latest Insights and Expertise from Aviabird's Tech Blog Hub."</p>
+            </div>
+            <Transition
+                fallback=move || view! {cx, 
+                  <div class="w-full flex justify-center">
+                    <span class="animate-ping absolute inline-flex h-7 w-7 rounded-full bg-sky-400 opacity-75"></span>
+                  </div>
                 }
-            </div>  
+            >
+                <div class="grid gap-8 lg:grid-cols-2">
+                    {
+                        move || {
+                          load_feeds
+                            .read(cx)
+                            .unwrap_or(vec![])
+                            .into_iter()
+                            .map(|feed| view! {cx,
+                              <FeedItem feed=feed />
+                            })
+                            .collect_view(cx)
+                        }
+                    }
+                </div>
+            </Transition>
         </div>
       </section>
     }
+}
+
+#[component]
+fn FeedItem(cx: Scope, feed: Feed) -> impl IntoView {
+  use chrono::{NaiveDateTime};
+
+  let parsed_date = NaiveDateTime::parse_from_str(&feed.pubDate, "%Y-%m-%d %H:%M:%S")
+      .expect("Failed to parse date string");
+
+  let date_time = chrono::DateTime::<chrono::Utc>::from_utc(parsed_date, chrono::Utc);
+  let formatted_date = date_time.format("%d %b").to_string();
+
+  
+  view! {cx,
+    <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex justify-between items-center mb-5 text-gray-500">
+            <span class="bg-indigo-100 text-indigo-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-800">
+                <i class="fa-regular fa-newspaper fa-xl pr-2" />
+                "Article"
+            </span>
+            <span class="text-sm">{formatted_date}</span>
+        </div>
+        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{feed.title}</a></h2>
+        <p class="mb-5 font-light text-gray-500 dark:text-gray-400" inner_html=blog_description(feed.description)></p>
+        <div class="flex justify-between items-center">
+            <div class="flex items-center space-x-4">
+                <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
+                <span class="font-medium dark:text-white">
+                  {feed.author}
+                </span>
+            </div>
+            <a href=feed.link class="inline-flex items-center font-medium text-indigo-600 dark:text-indigo-500 hover:underline">
+                "Read more"
+                <i class="fa-solid fa-arrow-right ml-2"></i>
+            </a>
+        </div>
+    </article>
+  }
 }
 
 fn blog_description(description: String) -> String {
